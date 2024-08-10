fd759d730b00d989d784874b62bf0e65a08f5372
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-10 21:25:47 +0530

    Food Display added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
-
-        <App />
-
-    </BrowserRouter>
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 
 )
