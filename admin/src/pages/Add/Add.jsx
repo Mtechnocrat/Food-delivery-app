1736ae07536f661dcb3684648f80f99cbfd8a734
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-12 23:56:41 +0530

    add item structure ready

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Add.css';
+import { assets } from '../../assets/assets';
+const Add = () => {
+  return (
+    <div className='add'>
+        <form className='flex-col'>
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor="image">
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input type="file" id='image' hidden required />
+            </div>
+            <div className="add-product-name flex-col">
+                <p>Product name</p>
+                <input type="text" name='name' placeholder='Type here' />
+            </div>
+            <div className="add-product-description flex-col">
+                <p>Product description</p>
+                <textarea name="description" row="6" placeholder='Write content here' required></textarea>
+            </div>
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product Category</p>
+                    <select name="category">
+                        <option value="Salad">Salad</option>
+                        <option value="Rolls">Rolls</option>
+                        <option value="Deserts">Deserts</option>
+                        <option value="Sandwich">Sandwich</option>
+                        <option value="Cake">Cake</option>
+                        <option value="Pure Veg">Pure Veg</option>
+                        <option value="Pasta">Pasta</option>
+                        <option value="Noodles">Noodles</option>
+                    </select>
+                </div>
+                <div className="add-price flex-col">
+                    <p>Product price</p>
+                    <input type="Number" name='price' placeholder='$20' />
+                </div>
+            </div>
+            <button type='submit' className='add-btn'>ADD</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default Add
