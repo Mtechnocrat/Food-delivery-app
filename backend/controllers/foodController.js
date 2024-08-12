b400dcaec443242b6607303c22161c9bd718f6da
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-12 18:24:58 +0530

    remove function working

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -26,4 +26,29 @@ const addFood=async (req,res) => {
     
 }
 
-export {addFood} 
+//all food list
+const listFood=async(req,res)=>{
+    try {
+        const foods= await foodModel.find({});
+        res.json({success:true,data:foods})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+//remove food_item
+const removeFood=async (req,res) => {
+    try {
+        const food=await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:"Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {addFood,listFood,removeFood} 
