3d0abab3cb3d6a191b73d26f5157ae0491ffa8c6
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-12 15:28:06 +0530

    Basic backend server created

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+
+
+//app config
+const app=express()
+const port=4000;
+
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get("/" ,(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
