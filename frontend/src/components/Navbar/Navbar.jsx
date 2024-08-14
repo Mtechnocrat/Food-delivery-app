b13c6d8ab9d44b89c7f0c7481a086d89f3a9b907
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-15 02:01:48 +0530

    order page is linked with home page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ export const Navbar = ({setShowLogin}) => {
             :<div className='navbar-profile'>
                 <img src={assets.profile_icon} alt="" />
                 <ul className="navbar-profile-dropdown">
-                    <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                    <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
                     <hr />
                     <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
                 </ul>
