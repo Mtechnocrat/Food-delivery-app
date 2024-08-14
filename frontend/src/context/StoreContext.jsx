885ee0b70a5118d5f79940ed3c6b12fdb1f25713
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-14 13:41:36 +0530

    remove functionaliy and adding to backend works

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -12,16 +12,22 @@ const StoreContextProvider=(props)=>{
     const [food_list,setFoodList] = useState([]);
 
     
-    const addToCart = (itemId)=> {
+    const addToCart = async (itemId)=> {
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
         }else{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
+        if(token){
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}});
+        }
     }
 
-    const removeFromCart = (itemId)=>{
+    const removeFromCart = async (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        if(token){
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount=()=>{
@@ -39,6 +45,7 @@ const StoreContextProvider=(props)=>{
         const response=await axios.get(url+"/api/food/list");
         setFoodList(response.data.data);
     }
+
 
     useEffect(()=>{
 
