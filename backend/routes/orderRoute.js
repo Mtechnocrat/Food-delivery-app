0e37bb2406a4aee2fbd9fb06a28adb97f04cd912
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-14 16:14:21 +0530

    verify functionality added

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authMiddelware from "../middleware/auth.js";
-import { placeOrder } from "../controllers/orderController.js";
+import { placeOrder, verifyOrder } from "../controllers/orderController.js";
 
 
 const orderRouter=express.Router();
 
 orderRouter.post("/place",authMiddelware,placeOrder)
+orderRouter.post("/verify",verifyOrder)
 
 export default orderRouter;
