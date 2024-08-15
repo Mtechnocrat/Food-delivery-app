3e848583e64f519be4d41d20278d72b0a4a44ec0
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-15 22:45:42 +0530

    Update status api added

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddelware from "../middleware/auth.js";
-import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 
 const orderRouter=express.Router();
@@ -9,5 +9,6 @@ orderRouter.post("/place",authMiddelware,placeOrder)
 orderRouter.post("/verify",verifyOrder)
 orderRouter.post("/userorders",authMiddelware,userOrders)
 orderRouter.get("/list",listOrders)
+orderRouter.post("/status",updateStatus)
 
 export default orderRouter;
