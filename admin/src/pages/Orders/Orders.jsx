e7eb4fc5290e9ad1956b30e5ed51f2076c902e01
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-15 23:02:22 +0530

    Project Completed

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -17,7 +17,16 @@ const Orders = ({url}) => {
       toast.error("Error")
     }
   }
-
+  const statusHandler=async(event,orderId)=>{
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    })
+    if(response.data.success){
+      await fetchAllOrders();
+      
+    }
+  }
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -48,7 +57,7 @@ const Orders = ({url}) => {
             </div>
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event)=>statusHandler(event,order._id)}  value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
