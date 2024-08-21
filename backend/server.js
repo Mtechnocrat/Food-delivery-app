118d6b8101d40f44b247d128e52526b33c5183b2
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-22 00:02:26 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,12 @@ app.get("/" ,(req,res)=>{
     res.send("API Working")
 })
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 4000;
+    app.listen(port,()=>{
+        console.log(`Server started on http://localhost:${port}`)
+    })
+}
+
+export default app;
 
