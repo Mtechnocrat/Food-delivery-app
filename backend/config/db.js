a83c1d70ce15474b4b657d0a4611216fc7b94c48
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-08-26 15:22:24 +0530

    updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://mrgenixx:1234@cluster0.thkkz.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
 }
